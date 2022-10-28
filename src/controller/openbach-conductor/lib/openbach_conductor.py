fd188770abb6df1688aabd3769de7916ab6e715c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-28 01:00:22 +0000

    Adding tarfile member sanitization to extractall()

--- src/controller/openbach-conductor/lib/openbach_conductor.py
+++ src/controller/openbach-conductor/lib/openbach_conductor.py
@@ -1229,7 +1229,26 @@ class AddTarJob(JobAction):
         path = '/opt/openbach/controller/src/jobs/private_jobs/{}'.format(self.name)
         try:
             with tarfile.open(self.path) as tar_file:
-                tar_file.extractall(path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar_file, path)
         except tarfile.ReadError as err:
             raise errors.ConductorError(
                     'Failed to uncompress the provided tar file',
